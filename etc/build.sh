3b21d45b46196b6ff56bc33100a8c4ae3090c460
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-07-02 13:16:48 -0300

    feat: add files and changes required for deployment to heroku

--- etc/build.sh
+++ etc/build.sh
@@ -2,8 +2,10 @@ rimraf dist
 
 export NODE_ENV=production
 
-tsc -p ./tsconfig.build.json --pretty
+tsc -p ./tsconfig.json --pretty
 
 cp -R src/public dist/src/public
+
+cp -R src/views dist/src/views
 
 cp package.json dist/
--- src/utils/db_connect.ts
+++ src/utils/db_connect.ts
@@ -1,6 +1,6 @@
 import { connect, connection } from 'mongoose'
 
-const mongoDB = process.env.DB_URL ?? 'mongodb://127.0.0.1/my_database'
+const mongoDB = process.env.MONGODB_URI ?? 'mongodb://127.0.0.1/my_database'
 
 connect(mongoDB)
 
